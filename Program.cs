921cbe90785bb6a342a5cf4f5a69b4fbeb7708e7
Kyle Johnston <kyle.johnston@entelect.co.za>
2024-02-06 08:47:09 +0200

    Menu improvements

--- Program.cs
+++ Program.cs
@@ -6,32 +6,50 @@ class Program
     {
         "Basic",
         "Semi",
-        "Table"
+        "Table",
+        "All",
+        "End"
     };
     static void Main(string[] args)
     {
-        
+        MenuSelection();
+    }
+
+    static void MenuSelection() {
         string Strategy = ProvideMenu();
         switch (Strategy) {
             case "Basic":
-                StartGame(new BasicStrategy());
+                StartGame(new List<IStrategy>() { new BasicStrategy() } );
                 break;
             case "Semi":
-                StartGame(new SemiStrategy());
+                StartGame(new List<IStrategy>() { new SemiStrategy() });
                 break;
             case "Table":
-                StartGame(new TableStrategy());
+                StartGame(new List<IStrategy>() { new TableStrategy() });
+                break;
+            case "All":
+                StartGame(new List<IStrategy>() { new BasicStrategy(), new SemiStrategy(), new TableStrategy() });
+                break;
+            case "End":
+                System.Environment.Exit(0);
                 break;
         }
     }
 
-    static void StartGame(IStrategy strategy) {
+    static void StartGame(List<IStrategy> strategies) {
         IRunner runner = new Runner();
-        
-        Game game = new Game(runner, strategy);
-        GameManager gameManager = new GameManager(game);
-        gameManager.RunSimulation(1000);
-        PrintStatistics(gameManager);
+        foreach (var strategy in strategies)
+        {
+            Game game = new Game(runner, strategy);
+            GameManager gameManager = new GameManager(game);
+            gameManager.RunSimulation(1000);
+            Console.WriteLine("Statistics for " + strategy.GetType());
+            PrintStatistics(gameManager);
+            Console.WriteLine();
+        }
+        Console.WriteLine("Press any key to return to menu");
+        Console.ReadLine();
+        MenuSelection();
     }
 
     static string ProvideMenu() {
@@ -61,6 +79,7 @@ class Program
             bool isSelected = strategy == Strategies[index];
             Console.WriteLine($"{(isSelected ? "> " : "  ")}{strategy}");
         }
+        Console.WriteLine();
     }
 
     private static void PrintStatistics(IStatistics statistics) {
